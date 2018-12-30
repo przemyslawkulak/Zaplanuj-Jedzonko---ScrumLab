68ab2ea6eb20a245467f883c03625077ee9e4359
Grzegorz 'ZnaFfca' Sałaciński <salacinski.grzegorz@gmail.com>
2018-12-30 22:00:32 +0100

    Działająca karuzela, częściowo ustwione widoki

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -1,7 +1,10 @@
+import random
 from datetime import datetime
 
 from django.shortcuts import render
 from django.views import View
+
+from jedzonko.models import Recipe
 
 
 class IndexView(View):
@@ -9,3 +12,10 @@ class IndexView(View):
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
         return render(request, "test.html", ctx)
+
+
+# nie działała próba uruchomienia wyświetlania szablonu za pomocą pętli
+
+def carousel(request):
+    recipes = random.sample(list(Recipe.objects.all()), 3)
+    return render(request, 'index.html', {'first': recipes[0], 'second': recipes[1], 'last': recipes[2]})
--- scrumlab/urls.py
+++ scrumlab/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from jedzonko.views import IndexView
+from jedzonko.views import IndexView, carousel
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', IndexView.as_view()),
+    path('', carousel, name='landing-page')
 ]
