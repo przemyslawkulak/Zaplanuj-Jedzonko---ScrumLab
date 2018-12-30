ef2618dc606f3f2d14d9013ae4ce48c0f5ca978c
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2018-12-30 17:16:07 +0100

    Podsumowanie planów na głównej stronie

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -3,9 +3,16 @@ from datetime import datetime
 from django.shortcuts import render
 from django.views import View
 
+from jedzonko.models import Plan
+
 
 class IndexView(View):
 
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
         return render(request, "test.html", ctx)
+
+
+def main_page(request):
+    ctx = Plan.objects.all().count()
+    return render(request, "dashboard.html", {'plans_count': ctx})
