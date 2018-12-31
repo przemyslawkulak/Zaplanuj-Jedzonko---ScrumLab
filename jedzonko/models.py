41cef6fefe167b03a02148edb81c501656e91d02
Grzegorz 'ZnaFfca' Sałaciński <salacinski.grzegorz@gmail.com>
2018-12-31 10:47:07 +0100

    Delete comments

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -1,6 +1,5 @@
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator
 from django.db import models
-
 
 
 class Recipe(models.Model):
@@ -15,40 +14,3 @@ class Recipe(models.Model):
     def __str__(self):
         return f'{self.name}, {self.ingredients}, {self.description}'
 
-
-class Plan(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    created = models.DateField(auto_now_add=True)
-
-    def __str__(self):
-        return f'{self.name}, \t{self.description}, \tcreated {self.created}'
-
-
-class DayName(models.Model):
-    day_type = models.CharField(max_length=16)
-    order = models.IntegerField()
-
-    def __str__(self):
-        return f'{self.day_type} : {self.order}'
-
-
-class RecipePlan(models.Model):
-    meal_name = models.CharField(max_length=255)
-    order = models.IntegerField()
-    day_name_id = models.ForeignKey(DayName, on_delete=models.CASCADE, related_name="dayname")
-    plan_id = models.ForeignKey(Plan, on_delete=models.CASCADE, related_name="plan")
-    recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="recipe")
-
-
-def __str__(self):
-    return f'{self.meal_name}, order: {self.order}'
-
-
-class Page(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    slug = models.SlugField(max_length=255, unique=True)
-
-    def __str__(self):
-        return f'{self.title}, \t{self.description}'
