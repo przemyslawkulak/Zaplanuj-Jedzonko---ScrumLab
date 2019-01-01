ba1abae8253acc8e777fd913aebe28b4a0bff3f3
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2019-01-01 14:25:14 +0100

    Podumowanie ilości planów i przepisów na stronie głównej

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from django.shortcuts import render
 from django.views import View
 
+from jedzonko.models import Plan, Recipe
+
 
 class IndexView(View):
 
@@ -10,5 +12,8 @@ class IndexView(View):
         ctx = {"actual_date": datetime.now()}
         return render(request, "test.html", ctx)
 
-def recipe_list(request):
-    return render(request, "recipes.html")
+
+def main_page(request):
+    ctx_plan = Plan.objects.all().count()
+    ctx_recipe = Recipe.objects.all().count()
+    return render(request, "dashboard.html", {'plans_count': ctx_plan, 'recipe_count': ctx_recipe})
