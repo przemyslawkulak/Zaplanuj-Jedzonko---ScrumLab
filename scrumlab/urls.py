fb300978eae2733443d65279e102330a0ddbdc20
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2019-01-03 13:05:29 +0100

    zadanie 5.3 wykonane

--- scrumlab/urls.py
+++ scrumlab/urls.py
@@ -14,17 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
-from jedzonko.views import IndexView, main_page, recipe_list, carousel, contact_link, about_link, index_link
+from jedzonko.views import IndexView, main_page, recipe_list, carousel, contact_link, about_link, index_link, \
+    recipe_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', IndexView.as_view()),
     path('', carousel, name='landing-page'),
-    path('main/', main_page),
+    path('main/', main_page, name="main-page"),
     path('recipe/list/', recipe_list, name="recipe-list"),
     path('contact/', contact_link, name="contact"),
     path('about/', about_link, name="about"),
     path('index/', index_link, name="index"),
+    re_path(r'^recipe/(?P<id>(\d)+)', recipe_detail)
 ]
